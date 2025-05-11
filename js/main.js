3163cd482d692978582c9139906ad3d0450ec180
sarah-1999 <sarasamyrashad1999@gmail.com>
2025-05-12 00:43:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerHTML ="hellow from another code" 
+document.getElementById("demo").style.backgroundColor = "rgb(255,0,0)";
+document.getElementById("demo").style.textAlign = 'center';
